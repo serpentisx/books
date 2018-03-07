f0ee5e33d92b3fb38a067ac5982972eacd49f01c
serpentisx <huy_van1@live.com>
2018-03-07 17:08:53 +0000

    Changed schema

--- schema.sql
+++ schema.sql
@@ -2,18 +2,18 @@ CREATE TABLE users (
     id serial primary key,
     username character varying(255) NOT NULL UNIQUE CHECK (char_length(username) >= 3),
     passwordhash text NOT NULL,
-    name character varying(255) NOT NULL,
+    name character varying(255) NOT NULL CHECK (name <> ''),
     imagepath text
 );
 
 CREATE TABLE categories (
     id serial primary key,
-    category character varying(255) NOT NULL UNIQUE
+    category character varying(255) NOT NULL UNIQUE CHECK (category <> '')
 );
 
 CREATE TABLE books (
     id serial primary key,
-    title character varying(255) NOT NULL UNIQUE,
+    title character varying(255) NOT NULL UNIQUE CHECK (title <> ''),
     isbn13 character varying(13) NOT NULL UNIQUE CHECK (ISBN13 ~ '^[0-9 ]*$'),
     author character varying(255),
     description text,
@@ -21,7 +21,10 @@ CREATE TABLE books (
     isbn10 character varying(10),
     datetime timestamp with time zone,
     pagecount integer CHECK (pagecount > 0),
-    language character varying(2)
+    language character varying(2),
+    imgUrl text,
+    price integer CHECK (price >= 0),
+    bsRank integer CHECK (bsRank >= 0)
 );
 
 CREATE TABLE review (
