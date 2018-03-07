129e7a0f2d033166445cd5f400573b3fd1d7f3c9
serpentisx <huy_van1@live.com>
2018-03-07 12:27:27 +0000

    Added users queries

--- queries/usersDb.js
+++ queries/usersDb.js
@@ -5,15 +5,18 @@ const { Client } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
 
+const offset = 0;
+const limit = 10;
+
 async function query(q, values = []) {
   const client = new Client({ connectionString });
 
   await client.connect();
 
   try {
-    const cleanedData = values.map(data => xss(data))
+    const cleanedData = values.map(data => xss(data));
     const result = await client.query(q, cleanedData);
-    
+
     return result;
   } catch (err) {
     console.error('Error running query');
@@ -22,3 +25,42 @@ async function query(q, values = []) {
     await client.end();
   }
 }
+
+
+async function selectAllUsers() {
+  const result = query('SELECT id, username, name, imagepath FROM users ORDER BY name OFFSET $1 LIMIT $2', [offset, limit]);
+
+  return result.rows[0];
+}
+
+async function selectUserById(id) {
+  const result = query('SELECT id, username, name, imagepath FROM users WHERE id = $1', [id]);
+
+  return result.rows[0];
+}
+
+async function selectAllReviewsByUserId(id) {
+  const result = query('SELECT * FROM review WHERE userid = $1 ORDER BY bookid OFFSET $2 LIMIT $3', [id, offset, limit]);
+
+  return result.rows[0];
+}
+
+async function insertReview({ userid, bookid, rating, review } = {}) {
+  const data = [userid, bookid, rating, review];
+  const result = query('INSERT INTO review (userid, bookid, rating, review) VALUES ($1, $2, $3, $4) RETURNING *', data);
+
+  return result.rows[0];
+}
+
+async function deleteReviewById(id) {
+  const result = query('DELETE FROM review where id = $1', [id]);
+
+  return result.rowCount === 1;
+}
+
+module.exports = {
+  selectAllUsers,
+  selectUserById,
+  selectAllReviewsByUserId,
+  deleteReviewById,
+};
