2fb74d298d7c28a4bb6f2aa782aac4f3a6b3d5f6
Bjarki Viðar Kristjánsson <bvk1@hi.is>
2018-03-16 15:25:54 +0000

    accountDb.js added

---
+++ queries/accountDb.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const xss = require('xss');
+
+const { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+async function query(q, values = []) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  try {
+    const cleanedData = values.map(data => xss(data));
+    const result = await client.query(q, cleanedData);
+
+    return result;
+  } catch (err) {
+    console.error('Error running query');
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+async function register({
+  username,
+  passwordhash,
+  name,
+  imagepath,
+} = {}) {
+  const data = [username, passwordhash, name, imagepath];
+  const result = query('INSERT INTO users(username, passwordhash, name, imagepath) VALUES($1, $2, $3, $4) RETURNING *', data);
+
+  return result.rows[0];
+}
+
+module.exports = {
+  register,
+};
