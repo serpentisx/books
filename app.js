5946dc57d794411b3575f701de044c0924b75822
Huy Nguyen <huy_van1@live.com>
2018-04-18 15:51:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
